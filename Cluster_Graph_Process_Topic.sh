1d36df2c0fb88cf566f23b8abecf9c694cd5f7b5
Fatma.s.gadelrab <f_s_g2001@yahoo.co.uk>
2018-05-24 00:09:18 +0200

    CJ-3 split

--- Cluster_Graph_Process_Topic.sh
+++ Cluster_Graph_Process_Topic.sh
@@ -4,7 +4,7 @@
 ###SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 ###SBATCH --ntasks=1
-#SBATCH --array=1-3
+#SBATCH --array=0-2
 #SBATCH --cpus-per-task=1
 ###SBATCH --exclusive
 #SBATCH --exclude=comp0[01-19]
@@ -33,9 +33,9 @@ all_gpus=1
 cuda=${SLURM_ARRAY_TASK_ID}
 
 processor='cpu'
-dataset='cs'
+dataset='split'
 
-singularity exec ~/data/containers/TensorFlow/tf-gpu.img ~/cpu/cs/CJ-3/Cluster_Graph_Process_Topic.py ${SLURM_ARRAY_TASK_ID} $all_gpus $cuda $processor $dataset
+singularity exec ~/data/containers/TensorFlow/tf-gpu.img ~/cpu/split/CJ-3/Cluster_Graph_Process_Topic.py ${SLURM_ARRAY_TASK_ID} $all_gpus $cuda $processor $dataset
 
 
 
